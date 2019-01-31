516af7be71234ff0207a7afd46591999c24c2580
tfuquirk <timothyfquirk@gmail.com>
2019-01-31 13:27:10 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  let counter = 0
+
+  let body = document.querySelector("body")
+  body.addEventListener('keydown', e => {
+
+    if (e.key === codes[counter]) {
+      counter++
+
+      if (counter === codes.length) {
+        alert("Congrats! You got the code right!")
+      }
+    } else {
+      counter = 0
+    }
+  })
 }
